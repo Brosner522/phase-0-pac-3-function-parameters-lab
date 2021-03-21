a11a583133065c4241903967122a239446c06847
barak <rosnerbb@gmail.com>
2021-03-20 21:21:00 -0400

    Completed assginment

---
+++ index.js
@@ -0,0 +1,17 @@
+var name = Josh;
+var language = Ember.js;
+
+function introduction(name) {
+    return `Hi, my name is ${name}.`;
+}
+
+function introductionWithLanguage(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+var name = Gracie;
+var language = "JavaScript";
+
+function introductionWithLanguageOptional(name, language) {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
